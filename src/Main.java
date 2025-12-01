ebaa5944461a6275e56bdaeddc2acaf016f9cddb
Game <joshuaa521@nycstudents.net>
2025-12-01 04:44:38 -0500

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         read();
-
+        System.out.println("Violet: " + Scoreboard.Violet);
+        System.out.println("Indigo: " + Scoreboard.Indigo);
         System.out.println("Orange: " + Scoreboard.Orange);
         System.out.println("Yellow: " + Scoreboard.Yellow);
         System.out.println("Red: " + Scoreboard.Red);
-        System.out.println("Blue: " + Scoreboard.Blue);
         System.out.println("Green: " + Scoreboard.Green);
-        System.out.println("Violet: " + Scoreboard.Violet);
-        System.out.println("Indigo: " + Scoreboard.Indigo);
+        System.out.println("Blue: " + Scoreboard.Blue);
     }
 
     public static void read() throws FileNotFoundException {
@@ -22,16 +21,20 @@ public class Main {
         Scoreboard names = new Scoreboard();
 
         while (s.hasNext()) {
+
             String a = "";
             String b = "";
+
             if (!s.hasNextInt()) {
                 a = s.next();
                 b = s.next();
                 names.constructor(a, b);
             }
+
             int points = s.nextInt();
             Scoreboard.recordPlay(points, a, b);
         }
+
         s.close();
     }
 }
